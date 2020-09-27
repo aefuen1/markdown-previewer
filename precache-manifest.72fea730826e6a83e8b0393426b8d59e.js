9f0b5bdb38809ae91c0d4bc920125dbb29f879aa
aefuen1 <aefuen1@gmail.com>
2020-09-27 03:11:47 -0400

    Updates

---
+++ precache-manifest.72fea730826e6a83e8b0393426b8d59e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70acc83b0b85339868d5f85794086501",
+    "url": "/markdown-previewer/index.html"
+  },
+  {
+    "revision": "8057916ba41c27a4c745",
+    "url": "/markdown-previewer/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "8989edb6850278fce0b5",
+    "url": "/markdown-previewer/static/css/main.2c402d78.chunk.css"
+  },
+  {
+    "revision": "8057916ba41c27a4c745",
+    "url": "/markdown-previewer/static/js/2.e9d2d577.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/markdown-previewer/static/js/2.e9d2d577.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8989edb6850278fce0b5",
+    "url": "/markdown-previewer/static/js/main.5abadd12.chunk.js"
+  },
+  {
+    "revision": "380a9cf99d31db8da525",
+    "url": "/markdown-previewer/static/js/runtime-main.4fd0b226.js"
+  }
+]);
